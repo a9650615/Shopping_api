41f275c23bfad6c02a68023425115eb9349af057
菜鳥祐 <a9650615@gmail.com>
2017-11-19 13:23:01 +0800

    sequelize init

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+require('dotenv').config()
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const sequelize = require('sequelize');
+const ENV = process.env;
+const Sequelize = new sequelize(ENV.MYSQL_DB, ENV.MYSQL_USER, ENV.MYSQL_PASS, {
+  host: ENV.MYSQL_HOST,
+  dialect: 'mysql'
+});
+
 const app = new Koa();
+
+Sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 app.use(koaBody());
 // response
